217053c78f4f987e596ab7cc3ea657dedaef1d99
Quentin Marlats <quentin.marlats@qmarlats.com>
2016-05-11 09:57:40 +0200

    Added benjam/pierre/louis files

---
+++ pyquizz/pierre-louis.py
@@ -0,0 +1,26 @@
+#pierre#louis#
+
+print("bonjour nous allons jouer ensemble a un blindtest\nle but du jeu sera sera de trouver le titre de la musique écoutée:\n")
+
+print(" a-jouer maintenant\n b-voir ses scores\n c-régles du jeux\n d-quiter\n")
+
+print("entrer votre choix:")
+
+choix = input()
+
+if choix == "a":
+		
+    print("fonction 1")
+		
+elif choix == "b":
+
+    print("fonction 2")
+	
+elif choix == "c":
+				
+    print("régles du jeux")
+
+elif choix == "d":
+	    exit()	
+else:
+    print("ce choix n'existe pas")
